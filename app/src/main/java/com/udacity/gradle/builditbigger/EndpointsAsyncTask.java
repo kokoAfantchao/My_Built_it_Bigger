6a5127a0d31c75ded63f76c64d2f4f2c2ea25e37
nexty <kokonestor@gmail.com>
2017-12-12 13:08:58 -0500

    12/12/17

--- app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
@@ -5,6 +5,12 @@ import android.os.AsyncTask;
 import android.support.v4.util.Pair;
 import android.widget.Toast;
 
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+;
+
 import java.io.IOException;
 
 /**
@@ -12,13 +18,13 @@ import java.io.IOException;
  */
 
 public class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, String> {
-    private static MyApi myApiService = null;
+    private static MyApi MyApiService = null;
     private Context context;
 
     @Override
     protected String doInBackground(Pair<Context, String>... params) {
-        if(myApiService == null) {  // Only do this once
-            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+        if(MyApiService == null) {  // Only do this once
+            MyApi.Builder builder = new MyApi().Builder(AndroidHttp.newCompatibleTransport(),
                     new AndroidJsonFactory(), null)
                     // options for running against local devappserver
                     // - 10.0.2.2 is localhost's IP address in Android emulator
@@ -32,14 +38,14 @@ public class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, S
                     });
             // end options for devappserver
 
-            myApiService = builder.build();
+            MyApiService = builder.build();
         }
 
         context = params[0].first;
         String name = params[0].second;
 
         try {
-            return myApiService.sayHi(name).execute().getData();
+            return MyApiService.sayHi(name).execute().getData();
         } catch (IOException e) {
             return e.getMessage();
         }
